24fd3171e9e08f38c21ff32ba573c2d0e70e17b3
Will Tsotetsi <will.tsotetsi@hotmail.com>
2025-09-07 18:54:21 +0200

    Correction...

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -46,4 +46,7 @@ urlpatterns = [
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
     path('admin/', views.admin_view, name='admin_view'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', views.delete_book, name='delete_book')
 ]
