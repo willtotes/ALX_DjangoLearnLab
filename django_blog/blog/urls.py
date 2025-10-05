b8d56cabc629fc5b3b607def7ebdf17ad6f629dd
Will Tsotetsi <will.tsotetsi@hotmail.com>
2025-10-05 03:56:09 +0200

    Add edited urls.py file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from .views import PostListView, PostDetailView, PostDeleteView, PostCreateView, PostUpdateView, CommentCreateView, CommentUpdateView, CommentDeleteView, SearchResultsView, TaggedPostsView
+from .views import PostListView, PostDetailView, PostDeleteView, PostCreateView, PostUpdateView, CommentCreateView, CommentUpdateView, CommentDeleteView, SearchResultsView, PostByTagListView
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -17,12 +17,12 @@ urlpatterns = [
     path('logout/', views.custom_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
 
-    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 
     path('search/', SearchResultsView.as_view(), name='search'),
-    path('tags/<str:tag>/', TaggedPostsView.as_view(), name='tagged-posts'),
+    path('tags/<str:tag_slug>/', PostByTagListView.as_view(), name='tagged-posts'),
     path('tags/', views.popular_tags, name='popular-tags'),
 
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -195,16 +195,16 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        form.instance.post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs['pk'])
         messages.success(self.request, 'Your comment has been added!')
         return super().form_valid(form)
     
     def get_success_url(self):
-        return reverse_lazy('post-detail', kwargs={'pk': self.kwargs['post_id']})
+        return reverse_lazy('post-detail', kwargs={'pk': self.kwargs['pk']})
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['post'] = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        context['post'] = get_object_or_404(Post, pk=self.kwargs['pk'])
         return context
 
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
@@ -277,7 +277,7 @@ class SearchResultsView(ListView):
         context['results_count'] = context['posts'].count()
         return context
     
-class TaggedPostsView(ListView):
+class PostByTagListView(ListView):
     model = Post
     template_name = 'blog/tagged_posts.html'
     context_object_name = 'posts'
