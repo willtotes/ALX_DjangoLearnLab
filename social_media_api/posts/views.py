cdf3c35891f169986a916e53f2e9eaa75b16ebe0
Will Tsotetsi <will.tsotetsi@hotmail.com>
2025-10-16 16:44:21 +0200

    Corrections posts/views.py/

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -279,7 +279,7 @@ class PostLikeView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk=None):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
 
         like, created = Like.objects.get_or_create(user=request.user, post=post)
         if created:
